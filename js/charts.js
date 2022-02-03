fc86ae538510e5a6515efcfd84dffc77a70ccc29
attiya waqqas <attiyawaqqas@gmail.com>
2022-02-02 22:54:07 -0500

    BellyButtonChallenge

--- js/charts.js
+++ js/charts.js
@@ -11,6 +11,7 @@ function init() {
         .append("option")
         .text(sample)
         .property("value", sample);
+        {console.log(key + ': ' + value);}
     });
 
     // Use the first sample from the list to build the initial plots
@@ -29,7 +30,6 @@ function optionChanged(newSample) {
   buildCharts(newSample);
   
 }
-
 // Demographics Panel 
 function buildMetadata(sample) {
   d3.json("samples.json").then((data) => {
@@ -38,16 +38,18 @@ function buildMetadata(sample) {
     var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
     var result = resultArray[0];
     // Use d3 to select the panel with id of `#sample-metadata`
-    var PANEL = d3.select("#sample-metadata");
+    var panel = d3.select("#sample-metadata");
 
     // Use `.html("") to clear any existing metadata
-    PANEL.html("");
+    panel.html("");
 
     // Use `Object.entries` to add each key and value pair to the panel
     // Hint: Inside the loop, you will need to use d3 to append new
     // tags for each key-value in the metadata.
     Object.entries(result).forEach(([key, value]) => {
-      PANEL.append("h6").text(`${key}: ${value}`);
+      panel.append("h6").text(`${key}: ${value}`);
+      {console.log(key + ': ' + value);}
+      
     });
 
   });
@@ -66,80 +68,108 @@ function buildCharts(sample) {
     var result = filterArray[0];
 
     // 6. Create variables that hold the otu_ids, otu_labels, and sample_values.
-    var sample_values = result.sample_values;
-    var otu_ids = result.otu_ids;
-    var otu_labels = result.otu_labels; 
+    var  ids = result.otu_ids;
+    var labels = result.otu_labels.slice(0, 10).reverse();
+    var values = result.sample_values.slice(0,10).reverse();
+
+    var bubbleLabels = result.otu_labels;
+    var bubbleValues = result.sample_values; 
 
     // 7. Create the yticks for the bar chart.
     // Hint: Get the the top 10 otu_ids and map them in descending order  
     //  so the otu_ids with the most bacteria are last. 
+    var yticks = ids.map(sampleObj => "OTU " + sampleObj).slice(0,10).reverse();
 
+    console.log(yticks)
     
 
     // 8. Create the trace for the bar chart. 
-   var barData = [{
-            x: sample_values.slice(0, 10).reverse(),
-            y: otu_ids.slice(0, 10).map(otu_id => `OTU ${otu_id}`).reverse(),
-            text: otu_labels.slice(0, 10).reverse(),
-            type: 'bar',
-            orientation: 'h',
-            marker: {
-                color: 'rgb(242, 113, 102)'
-            },
-        }];
+    var barData = [{
+      x: values,
+      y: yticks,
+      type: "bar",
+      orientation: "h",
+      text: labels 
+    }];
     // 9. Create the layout for the bar chart. 
     var barLayout = {
       title: "Top 10 Microbial Species in Belly Buttons",
       xaxis: { title: "Bacteria Sample Values" },
       yaxis: { title: "OTU IDs" }
-  };
+   };
     // 10. Use Plotly to plot the data with the layout. 
-    Plotly.newPlot('bar', barData, barLayout);
-    
-  });
-}
-  // Bar and Bubble charts
-// Create the buildCharts function.
-function buildCharts(sample) {
-  // Use d3.json to load and retrieve the samples.json file 
-   d3.json("samples.json").then((data) => {
-       var samples= data.samples;
-       var resultsarray= samples.filter(sampleobject => sampleobject.id == sample);
-       var result= resultsarray[0];
+
+   Plotly.newPlot('bar', barData, barLayout);
+   // 1. Create the trace for the bubble chart.
+   var bubbleData = [{
+    x: ids,
+    y: bubbleValues,
+    text: bubbleLabels,
+    mode: "markers",
+     marker: {
+       size: bubbleValues,
+       color: bubbleValues,
+       colorscale: "Portland" 
+     }
+  }];
+
+  // 2. Create the layout for the bubble chart.
+  var bubbleLayout = {
+      title: "Bacteria Cultures Per Sample",
+      xaxis: {title: "OTU ID"},
+      automargin: true,
+      hovermode: "closest"
+  };
+
+  // 3. Use Plotly to plot the data with the layout.
+ Plotly.newPlot("bubble", bubbleData, bubbleLayout);
+
+  // 4. Create the trace for the gauge chart.
   
-       var ids = result.otu_ids;
-       var labels = result.otu_labels;
-       var values = result.sample_values;
-      
-    // 1. Create the trace for the bubble chart.
-          var bubbleData = [
-         {
-          x: ids,
-          y: values,
-          text: labels,
-          mode: "markers",
-          markers: {
-          color: ids,
-          size: values,
-          }
-      }
-   
-    ];
+  var metadata = data.metadata;
+  var gaugeArray = metadata.filter(metaObj => metaObj.id == sample);  
 
-    // 2. Create the layout for the bubble chart.
-     var bubbleLayout = {
-      margin: { t: 0 },
-      xaxis: { title: "OTU ID" },
-      hovermode: "closest",
-      
-    };
+  // 2. Create a variable that holds the first sample in the metadata array.
+      var gaugeResult = gaugeArray[0];
 
-    // 3. Use Plotly to plot the data with the layout.
-    Plotly.newPlot('bubble', bubbleData, bubbleLayout);
+  // 3. Create a variable that holds the washing frequency.  
+  var wfreqs = gaugeResult.wfreq;
+  console.log(wfreqs)
 
-});
- 
+  // 4. Create the trace for the gauge chart.
+  var gaugeData = [{
+    value: wfreqs,
+    type: "indicator",
+    mode: "gauge+number",
+    title: {text: "<b> Belly Button Washing Frequency </b> <br></br> Scrubs Per Week"},
+    gauge: {
+      axis: {range: [null,10], dtick: "2"},
+
+      bar: {color: "black"},
+      steps:[
+        {range: [0, 2], color: "red"},
+        {range: [2, 4], color: "orange"},
+        {range: [4, 6], color: "yellow"},
+        {range: [6, 8], color: "lightgreen"},
+        {range: [8, 10], color: "green"}
+      ],
+      dtick: 2
+    }
+  }];
+  
+  // 5. Create the layout for the gauge chart.
+  var gaugeLayout = { 
+   automargin: true
+  };
+
+  // 6. Use Plotly to plot the gauge data and layout.
+  Plotly.newPlot(gauge, gaugeData, gaugeLayout);
+
+  });
+  
 }
+
+
 
 
   
